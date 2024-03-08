43b9dff89bc0274e1f2aa0729db49f2617e29660
pamellam <38889625+pamellam@users.noreply.github.com>
2024-03-08 16:07:15 +0200

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import EventsPage from './pages/EventsPage';
+import EventDetailPage from './pages/EventDetailPage';
+import NewEventPage from './pages/NewEventPage';
+import EditEventPage from './pages/EditEventPage';
+import ErrorEventPage from './pages/ErrorEventPage';
+import RootLayout from './pages/RootLayout';
+import EventsRootLayout from './pages/EventsRootLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorEventPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ':eventId', element: <EventDetailPage /> },
+          { path: 'new', element: <NewEventPage /> },
+          { path: ':eventId/edit', element: <EditEventPage /> },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Event 1',
+  },
+  {
+    id: 'e2',
+    title: 'Event 2',
+  },
+  {
+    id: 'e4',
+    title: 'Event 3',
+  },
+];
+
+const EventsPage = () => {
+  return (
+    <>
+      <h1>Events Page</h1>
+      <ul>
+        {DUMMY_EVENTS.map((event) => (
+          <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default EventsPage;
