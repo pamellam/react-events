40ae6a3204830e95d17a79fe13bdc0939c57ff92
pamellam <38889625+pamellam@users.noreply.github.com>
2024-03-09 12:35:02 +0200

    reflecting the current navigation state in the UI

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,11 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
+  /*navigation.state = check if we initated transiton,
+   or if we are in the middle of a transition, or its done */
+  const navigation = useNavigation();
+ 
   return (
     <>
       <MainNavigation />
       <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
